9385bc41ed314cc5d0ffe98ee91a51b267e85600
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-07 06:32:25 -0300

    features: root import, auth routes, background and input/button components

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import BackGround from '~/components/Brackground';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <BackGround>
+      <Text>SignUp</Text>
+    </BackGround>
+  );
+}
