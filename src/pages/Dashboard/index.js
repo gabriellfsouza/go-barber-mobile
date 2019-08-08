bd20f23f1678a938a0c01588a6c6b741cf8cad6a
Gabriel Lima <gabriellfsouza@gmail.com>
2019-08-08 05:30:34 -0300

    chore: route stylling

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Brackground';
+
 import { Container } from './styles';
 
 export default function Dashboard() {
-  return <Container />;
+  return (
+    <Background>
+      <Container />
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
